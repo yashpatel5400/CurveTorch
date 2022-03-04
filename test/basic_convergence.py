ddf30dcfe6c434ef5d8a0ff4ecb6284d3653ad13
yashpatel5400 <ypatel5400@gmail.com>
2022-03-03 21:31:22 -0500

    Check test

--- test/basic_convergence.py
+++ test/basic_convergence.py
@@ -3,5 +3,14 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-def test_answer():
-    assert 3 + 2 == 5
+import unittest
+
+class BasicConvergence(unittest.TestCase):
+    def test_sum(self):
+        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+    def test_sum_tuple(self):
+        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+
+if __name__ == '__main__':
+    unittest.main()
