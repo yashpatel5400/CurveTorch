3fcc262b279b4802cebd24e56df60f00d398245e
yashpatel5400 <ypatel5400@gmail.com>
2022-04-02 17:57:12 -0400

    Add contour trace plots

--- test/test_basic_convergence.py
+++ test/test_basic_convergence.py
@@ -6,13 +6,17 @@
 import pytest
 import torch
 import matplotlib.pyplot as plt
+import numpy as np
 
 import curvetorch as curve
 
 
+def basic_f(x, y):
+    return x * x + y * y
+
 def basic(tensor):
     x, y = tensor
-    return x * x + y * y
+    return basic_f(x, y)
 
 
 def rosenbrock(tensor):
@@ -28,7 +32,7 @@ def quadratic(tensor):
 
 
 cases = [
-    (basic, (-1.0, 0.5), (0, 0)),
+    (basic, (-0.3, 0.5), (0, 0)),
     (rosenbrock, (1.5, 1.5), (1, 1)),
     (quadratic, (0.5, 0.5), (0, 0)),
 ]
@@ -40,7 +44,7 @@ def ids(v):
 
 
 optimizers = [
-    (curve.CurveSGD, {'lr': 0.0015}, 15000),
+    (curve.CurveSGD, {'lr': 0.0015}, 10000),
 ]
 
 
@@ -53,6 +57,9 @@ def test_benchmark_function(case, optimizer_config):
     x = torch.Tensor(initial_state).requires_grad_(True)
     x_min = torch.Tensor(min_loc)
     optimizer = optimizer_class([x], **config)
+    x0s = []
+    x1s = []
+
     fs = []
     for _ in range(iterations):
         fs.append(func(x))
@@ -62,7 +69,24 @@ def test_benchmark_function(case, optimizer_config):
             f.backward(retain_graph=True, create_graph=True)
             return f
         optimizer.step(closure)
+
+        x0, x1 = x
+        x0s.append(float(x0.detach().numpy()))
+        x1s.append(float(x1.detach().numpy()))
+
     plt.plot(range(iterations), fs)
+    plt.show()
+
+    basic_f_vec = np.vectorize(basic_f)
+    a, b = np.meshgrid(np.linspace(-1, 1, 100),
+                       np.linspace(-1, 1, 100))
+
+    plt.contour(a, b, basic_f_vec(a, b))
+
+    for i in range(len(x0s)-1):
+        plt.plot(x0s[i:i+2], x1s[i:i+2],
+                 alpha=float(i) / (len(x0s)-1),
+                 color="blue")
     plt.show()
 
     assert torch.allclose(x, x_min, atol=0.01)
